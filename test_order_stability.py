547e950bad4bbadc2a721bbc99c69e37a4aa465c
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-02-17 19:12:55 -0500

    It looks like the USE_DOUBLE_QUERY=true strategy is perfect!

--- test_order_stability.py
+++ test_order_stability.py
@@ -265,6 +265,71 @@ def query_feature_page_arcgis(server_url, a_polygon, resultOffset=0, resultRecor
     traceback.print_exc()
     return []
 
+def query_all_feature_oids(server_url, a_polygon):
+  global last_feature_page_json, server_oid_field_name
+  if a_polygon is None:
+    return []
+  if hasattr(a_polygon, 'exterior'):
+    coords = [pt for pt in a_polygon.exterior.coords]
+  else:
+    coords = a_polygon
+  json_query_g = {'spatialReference': {'wkid': 4326}, 'rings': [coords]}
+  data = urllib.parse.urlencode({
+      'geometry': json.dumps(json_query_g),
+      'geometryType': 'esriGeometryPolygon',
+      #'outFields': '*',
+      'returnGeometry': False,
+      'returnIdsOnly': True,
+      'orderByFields': f'{server_oid_field_name} DESC',
+      'f': 'pjson',
+  }).encode()
+  req = urllib.request.Request(
+      server_url,
+      data=data)
+  resp = urllib.request.urlopen(req)
+  resp_txt = resp.read()
+  resp_json = json.loads(resp_txt)
+  if not 'objectIds' in resp_json:
+    if not isinstance(resp_txt, str):
+      resp_txt = resp_txt.decode('utf-8')
+    if not isinstance(data, str):
+      data = data.decode('utf-8')
+    print(f'WARNING ERROR JSON:\n{resp_txt}\n^^ query data={data}\n')
+    return []
+
+  # Update if info available
+  server_oid_field_name = resp_json.get('objectIdFieldName', server_oid_field_name)
+
+  return resp_json['objectIds']
+
+def query_feature(server_url, feature_oid):
+  global last_feature_page_json, server_oid_field_name
+  data = urllib.parse.urlencode({
+      'outFields': '*',
+      'returnGeometry': True,
+      'where': f'{server_oid_field_name} = {feature_oid}',
+      'f': 'pjson',
+  }).encode()
+  req = urllib.request.Request(
+      server_url,
+      data=data)
+  resp = urllib.request.urlopen(req)
+  resp_txt = resp.read()
+  resp_json = json.loads(resp_txt)
+  last_feature_page_json = resp_json # Save off for any reporting we want to do
+  # We return a list of objectids
+  if not 'features' in resp_json:
+    if not isinstance(resp_txt, str):
+      resp_txt = resp_txt.decode('utf-8')
+    if not isinstance(data, str):
+      data = data.decode('utf-8')
+    print(f'WARNING ERROR JSON:\n{resp_txt}\n^^ query data={data}\n')
+    return []
+  if len(resp_json['features']) < 1:
+    return None
+  else:
+    return next(x for x in resp_json['features'])
+
 def query_all_feature_pages(server_url, a_polygon, query_feature_page_fn=None):
   global last_feature_page_json
   if a_polygon is None:
@@ -276,7 +341,7 @@ def query_all_feature_pages(server_url, a_polygon, query_feature_page_fn=None):
   offset_and_len = list() # Tuple of (resultOffset, resultRecordCount)
   pages_of_oids = list()
   while allowed_zero_replies > 0:
-    result_record_count = random.choice([4,5,6,7,8,9,10])
+    result_record_count = random.choice([4,5,6,7,8,9,10,11,12])
     feature_page = query_feature_page_fn(
       server_url,
       a_polygon,
@@ -285,14 +350,6 @@ def query_all_feature_pages(server_url, a_polygon, query_feature_page_fn=None):
     )
     if len(feature_page) < 1:
       allowed_zero_replies -= 1
-      #if 'exceededTransferLimit' in last_feature_page_json:
-      #  print(f'NOTE: Zero response! resultOffset={result_offset} resultRecordCount={result_record_count}')
-      #  if last_feature_page_json['exceededTransferLimit']:
-      #    # features is [], AND exceededTransferLimit is True
-      #    # Hmmmm we really want the first feature. How can we get it?
-      #    if random.choice([True, False, False]):
-      #      allowed_zero_replies += 1
-      #    result_offset += 1 # We decide to SKIP the first feature; no idea what this will bring.
     else:
       # got some features, reset allowed_zero_replies!
       allowed_zero_replies = 6
@@ -306,7 +363,24 @@ def query_all_feature_pages(server_url, a_polygon, query_feature_page_fn=None):
 
   return offset_and_len, pages_of_oids
 
+def double_query_all_feature_pages(server_url, a_polygon, query_feature_page_fn=None):
+  global last_feature_page_json
+  if a_polygon is None:
+    return []
+  if query_feature_page_fn is None:
+    query_feature_page_fn = query_feature_page
 
+  all_oids = query_all_feature_oids(server_url, a_polygon)
+
+  offset_and_len = list() # Tuple of (resultOffset, resultRecordCount)
+  pages_of_oids = list()
+
+  for i in range(0, len(all_oids)):
+    f = query_feature(server_url, all_oids[i])
+    pages_of_oids.append([read_oid(f)])
+    offset_and_len.append((i, 1))
+
+  return offset_and_len, pages_of_oids
 
 if __name__ == '__main__':
   for server_url in server_urls:
@@ -324,7 +398,9 @@ if __name__ == '__main__':
     min_y = fc_extent.get('ymin', min_y)
     print(f'min_x={min_x} max_x={max_x} min_y={min_y} max_y={max_y}')
     use_arcgis_pages = len(os.environ.get('USE_ARCGIS_PAGES', '')) > 0
-    print(f'USE_ARCGIS_PAGES = {use_arcgis_pages} (when True, arcgis.features.FeatureLayer::query is used download the data. When False, urllib.request.Request downloads the data.)')
+    print(f'USE_ARCGIS_PAGES = {use_arcgis_pages}\n    (when True, arcgis.features.FeatureLayer::query is used download the data. When False, urllib.request.Request downloads the data.)')
+    use_double_query = len(os.environ.get('USE_DOUBLE_QUERY', '')) > 0
+    print(f'USE_DOUBLE_QUERY = {use_double_query}\n    (when True, double_query_all_feature_pages combines the data. When False, query_all_feature_pages combines the data.)')
 
     server_oid_field_name = read_fc_oid_field(server_url)
     if not server_oid_field_name in possible_oid_names:
@@ -346,24 +422,32 @@ if __name__ == '__main__':
     print(f'expected_oids({len(expected_oids)}) = {expected_oids}')
 
     # Step 2.5: Detect if ANY of the offsets will return 0 features; if so log those as "sticky query indexes" which are likely to cause the below to hang/omit data
-    num_sticky_indexes = 0
-    for i in range(0, len(expected_oids)):
-      page_with_one_item = query_feature_page(server_url, g, resultOffset=i, resultRecordCount=10)
-      if len(page_with_one_item) < 1:
-        print(f'Sticky Query Index at index {i} detected: Asked for resultOffset={i}, resultRecordCount=10 and recived {page_with_one_item}. We should have seen OID {expected_oids[i]}')
-        num_sticky_indexes += 1
-    print(f'num_sticky_indexes = {num_sticky_indexes} (if this is ==0, this means we could successfully query every item one-at-a-time)')
+    #num_sticky_indexes = 0
+    #for i in range(0, len(expected_oids)):
+    #  page_with_one_item = query_feature_page(server_url, g, resultOffset=i, resultRecordCount=10)
+    #  if len(page_with_one_item) < 1:
+    #    print(f'Sticky Query Index at index {i} detected: Asked for resultOffset={i}, resultRecordCount=10 and recived {page_with_one_item}. We should have seen OID {expected_oids[i]}')
+    #    num_sticky_indexes += 1
+    #print(f'num_sticky_indexes = {num_sticky_indexes} (if this is ==0, this means we could successfully query every item one-at-a-time)')
     print()
 
     # Step 3: Join pages together and do analysis on
     #   - do OIDS repeat?
     #   - Are OIDS omitted?
-    if use_arcgis_pages:
-      print(f'Querying pages using query_feature_page_arcgis')
-      offset_and_len, pages_of_oids = query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page_arcgis)
+    if use_double_query:
+      if use_arcgis_pages:
+        print(f'Querying pages using double_query_all_feature_pages + query_feature_page_arcgis')
+        offset_and_len, pages_of_oids = double_query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page_arcgis)
+      else:
+        print(f'Querying pages using double_query_all_feature_pages + query_feature_page')
+        offset_and_len, pages_of_oids = double_query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page)
     else:
-      print(f'Querying pages using query_feature_page')
-      offset_and_len, pages_of_oids = query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page)
+      if use_arcgis_pages:
+        print(f'Querying pages using query_all_feature_pages + query_feature_page_arcgis')
+        offset_and_len, pages_of_oids = query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page_arcgis)
+      else:
+        print(f'Querying pages using query_all_feature_pages + query_feature_page')
+        offset_and_len, pages_of_oids = query_all_feature_pages(server_url, g, query_feature_page_fn=query_feature_page)
 
     print(f'=== {len(pages_of_oids)} pages of oids returned ===')
     for i in range(0, min(len(pages_of_oids), len(offset_and_len)) ):
@@ -371,12 +455,16 @@ if __name__ == '__main__':
 
     pages_of_oids_unique_oids = set()
     flattened_returned_pages = list()
+    flattened_deduped_returned_pages = list()
     for p in pages_of_oids:
       for oid in p:
         pages_of_oids_unique_oids.add(oid)
         flattened_returned_pages.append(oid)
+        if not oid in flattened_deduped_returned_pages:
+          flattened_deduped_returned_pages.append(oid)
 
     print(f'pages_of_oids_unique_oids({len(pages_of_oids_unique_oids)}) = {pages_of_oids_unique_oids}')
+    print(f'flattened_deduped_returned_pages({len(flattened_deduped_returned_pages)}) = {flattened_deduped_returned_pages}')
 
     print()
     end_s = time.time()
@@ -398,7 +486,7 @@ if __name__ == '__main__':
       if count > 1:
         print(f'  Observation: {oid} was returned {count} times!')
         q1_is_true = True
-    print(f'Q1 is {tf_to_yn(q1_is_true)} for {server_host} running version {server_version} and USE_ARCGIS_PAGES = {use_arcgis_pages}')
+    print(f'Q1 is {tf_to_yn(q1_is_true)} for {server_host}')
     print()
 
     print('Q2: Are there expected OIDs which were NOT returnd by the paginated query?')
@@ -407,7 +495,7 @@ if __name__ == '__main__':
       if not e_oid in pages_of_oids_unique_oids:
         print(f'  Observation: {e_oid} was NOT returned in the pages!')
         q2_is_true = False
-    print(f'Q2 is {tf_to_yn(q2_is_true)} for {server_host} running version {server_version} and USE_ARCGIS_PAGES = {use_arcgis_pages}')
+    print(f'Q2 is {tf_to_yn(q2_is_true)} for {server_host}')
     print()
 
     print('Q3: Are OIDs returned by the paginated query which were NOT expected OIDs?')
@@ -416,16 +504,16 @@ if __name__ == '__main__':
       if not p_oid in expected_oids:
         print(f'  Observation: {p_oid} returned in the pages but NOT in the initial large query which produced the expected results!')
         q3_is_true = False
-    print(f'Q3 is {tf_to_yn(q3_is_true)} for {server_host} running version {server_version} and USE_ARCGIS_PAGES = {use_arcgis_pages}')
+    print(f'Q3 is {tf_to_yn(q3_is_true)} for {server_host}')
     print()
 
-    print('Q4: Is the ordering different from the one big query to the combination of smaller queries?')
+    print('Q4: Is the ordering different from the one big query to the combination of smaller queries? (We use flattened_deduped_returned_pages instead of flattened_returned_pages to answer this question)')
     q4_is_true = False
-    for i in range(0, min(len(expected_oids), len(flattened_returned_pages))):
-      if expected_oids[i] != flattened_returned_pages[i]:
-        print(f'  Expected OID {expected_oids[i]} at position {i} but flattened_returned_pages[{i}] = {flattened_returned_pages[i]}')
+    for i in range(0, min(len(expected_oids), len(flattened_deduped_returned_pages))):
+      if expected_oids[i] != flattened_deduped_returned_pages[i]:
+        print(f'  Expected OID {expected_oids[i]} at position {i} but flattened_deduped_returned_pages[{i}] = {flattened_deduped_returned_pages[i]}')
         q4_is_true = True
-    print(f'Q4 is {tf_to_yn(q4_is_true)} for {server_host} running version {server_version} and USE_ARCGIS_PAGES = {use_arcgis_pages}')
+    print(f'Q4 is {tf_to_yn(q4_is_true)} for {server_host}')
     print()
 
 
